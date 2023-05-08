825f07800323139c61590ef8683f4aa72794eeca
Jason Zhang <jasonjzhang17@gmail.com>
2023-05-08 11:16:09 -0400

    Functional starter messenger app

---
+++ src/components/Search.jsx
@@ -0,0 +1,117 @@
+import React, { useContext, useState } from "react";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  setDoc,
+  doc,
+  updateDoc,
+  serverTimestamp,
+  getDoc,
+} from "firebase/firestore";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
+
+const Search = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+
+  const { currentUser } = useContext(AuthContext);
+  const { dispatch } = useContext(ChatContext);
+
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, "webUsers"),
+      where("displayName", "==", username)
+    );
+
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (err) {
+      setErr(true);
+    }
+  };
+
+  // Run search when user hits enter
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
+
+  const handleSelect = async (u) => {
+    // check whether the group(chats in firestore) exists, if not create
+    const combinedId =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+
+    console.log("Ran Chats handleSelect", u)
+    dispatch({ type: "CHANGE_USER", payload: u });
+
+    try {
+      const res = await getDoc(doc(db, "chats", combinedId));
+
+      if (!res.exists()) {
+        // create a chat in chats collection
+        await setDoc(doc(db, "chats", combinedId), { messages: [] });
+        console.log("Created chat")
+
+        // create user chat for current user
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+        console.log("Created current user userChat")
+
+        // create user chat for other user
+        await updateDoc(doc(db, "userChats", user.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+        console.log("Created other user userChat")
+      }
+    } catch (err) {}
+
+    // Reset search query
+    setUser(null);
+    setUsername("")
+  };
+
+  return (
+    <div className="search">
+      <div className="searchForm">
+        <input
+          type="text"
+          placeholder="Find a user"
+          onKeyDown={handleKey}
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+        />
+      </div>
+      {err && <span>User not found!</span>}
+      {user && (
+        <div className="userChat" onClick={() => handleSelect(user)}>
+          <img src={user.photoURL} alt="" />
+          <div className="userChatInfo">
+            <span>{user.displayName}</span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Search;
