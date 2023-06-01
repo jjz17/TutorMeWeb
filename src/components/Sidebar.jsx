833b8aa6f23fe91cafdb0a068216aeaae28258b4
Jason Zhang <jasonjzhang17@gmail.com>
2023-06-01 12:35:09 -0400

    Create ticket list, currently not rendering ticket description

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Navbar from "./Navbar";
 import Search from "./Search";
 import Chats from "./Chats";
+import Tickets from "./Tickets";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <Navbar />
       <Search />
-      <Chats />
+      {/* <Chats /> */}
+      <Tickets />
     </div>
   );
 };
---
+++ src/components/Tickets.jsx
@@ -0,0 +1,84 @@
+import { doc, onSnapshot, collection, getFirestore, getDocs } from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
+import { db } from "../firebase";
+
+const Tickets = () => {
+  const [tickets, setTickets] = useState([]);
+
+  const { currentUser } = useContext(AuthContext);
+  const { dispatch } = useContext(ChatContext);
+
+  // useEffect depends on change in currentUser.uid
+  // useEffect(() => {
+  //   const getTickets = () => {
+  //     // Retrieves current user's userChats each time it is updated
+  //     const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+  //       setTickets(doc.data());
+  //       console.log("User's chats updated");
+  //     });
+
+  //     return () => {
+  //       unsub();
+  //     };
+  //   };
+
+  //   currentUser.uid && getTickets();
+  // }, [currentUser.uid]);
+
+  useEffect(() => {
+    const fetchTickets = async () => {
+      try {
+        const snapshot = await getDocs(collection(db, 'tickets'));
+        const ticketList = snapshot.docs.map(doc => ({
+          id: doc.id,
+          description: doc.data().description,
+          date: doc.data().date,
+          img: doc.data().img
+        }));
+        setTickets(ticketList);
+        console.log(ticketList)
+      } catch (error) {
+        console.error('Error fetching tickets:', error);
+      }
+    };
+
+    fetchTickets();
+  }, []);
+
+
+  const handleSelect = (u) => {
+    console.log("Chats handleSelect userInfo");
+    console.log(u);
+    dispatch({ type: "CHANGE_USER", payload: u });
+  };
+
+  return (
+    <div className="chats">
+      {tickets &&
+        Object.entries(tickets)
+          ?.sort((a, b) => b.date - a.date)
+          .map((ticket) => (
+            // {Object.entries(chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat) => (
+            <div
+              className="user-chat"
+              key={ticket.id}
+              // onClick={() => handleSelect(ticket[1].userInfo)}
+            >
+              <p>{ticket.description}</p>
+              
+              {/* {ticket[1].userInfo && (
+                <img src={ticket[1].userInfo.photoURL} alt="" />
+              )}
+              <div className="user-chat-info">
+                <span>{ticket[1].userInfo.displayName}</span>
+                <p>{ticket[1].lastMessage?.text}</p>
+              </div> */}
+            </div>
+          ))}
+    </div>
+  );
+};
+
+export default Tickets;
