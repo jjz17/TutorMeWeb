bd3a55da9b29d92c0af1305dcc5137a05a396291
Andrew Mascillaro <amascillaro@gmail.com>
2023-06-08 19:13:10 -0400

    Start profile authcontext fix

--- src/components/Tickets.jsx
+++ src/components/Tickets.jsx
@@ -11,11 +11,15 @@ import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/ChatContext";
 import { db } from "../firebase";
+import _ from "lodash";
 
 const Tickets = () => {
-  const [tickets, setTickets] = useState([]);
+  const [unclaimedTickets, setUnclaimedTickets] = useState([]); // tutors
+  const [myTickets, setMyTickets] = useState([]); // students and tutors
+  const [myOpenTickets, setMyOpenTickets] = useState([]); // students and tutors
+  const [myClosedTickets, setMyClosedTickets] = useState([]); // students and tutors
 
-  const { currentUser } = useContext(AuthContext);
+  const { currentUser, profile } = useContext(AuthContext);
   const { dispatch } = useContext(ChatContext);
 
   // Fetch open tickets on render
@@ -36,7 +40,7 @@ const Tickets = () => {
         // date: doc.data().date,
         // img: doc.data().img
         // }
-        setTickets(ticketList);
+        setUnclaimedTickets(ticketList);
         console.log(ticketList);
         // console.log(ticketList[0].description)
         // console.log(ticketList[0].id)
@@ -47,6 +51,47 @@ const Tickets = () => {
 
     fetchOpenTickets();
   }, []);
+
+  useEffect(() => {
+    const fetchMyTickets = async () => {
+      try {
+        var idType;
+        if (profile.role === "student") {
+          idType = "studentId";
+        } else {
+          idType = "tutorId";
+        }
+        const snapshot = await getDocs(
+          query(collection(db, "tickets"), where(idType, "==", currentUser.uid))
+        );
+        const ticketList = snapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        // {
+        // id: doc.id,
+        // description: doc.data().description,
+        // date: doc.data().date,
+        // img: doc.data().img
+        // }
+        setMyTickets(ticketList);
+        console.log(ticketList);
+        // console.log(ticketList[0].description)
+        // console.log(ticketList[0].id)
+      } catch (error) {
+        console.error("Error fetching tickets:", error);
+      }
+    };
+
+    fetchMyTickets();
+  }, [currentUser, profile]);
+
+  useEffect(() => {
+    var separatedTickets = _.partition(myTickets, (t) => t.status == "open");
+
+    setMyOpenTickets(separatedTickets[0]);
+    setMyClosedTickets(separatedTickets[1]);
+  }, myTickets);
 
   const handleSelect = (t) => {
     console.log("Selected ticket", t);
@@ -65,8 +110,8 @@ const Tickets = () => {
 
   return (
     <div className="chats">
-      {tickets &&
-        tickets
+      {unclaimedTickets &&
+        unclaimedTickets
           .sort((a, b) => b.date - a.date)
           .map((ticket) => (
             <div
